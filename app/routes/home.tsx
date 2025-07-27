eed1a0e2ffa1339cd9c3c1849d59c0d0c4537ab2
Timo Kuki <timo.kuki@studiogoldsmith.fi>
2025-07-27 17:06:33 +0300

    yup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,44 @@
+import { resumes } from "constants/index";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import { callbackify } from "util";
+import ResumeCard from "~/components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar></Navbar>
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+
+      {resumes.length > 0 && (
+
+        <div className="resumes-section">
+
+          {resumes.map((resume: Resume) => (
+            <ResumeCard key={resume.id} resume={resume}></ResumeCard>
+          ))}
+        </div>
+      )}
+
+    </section>
+
+
+/*https://youtu.be/iYOz165wGkQ?list=LL&t=2071*/
+
+
+
+
+
+  </main>;
 }
